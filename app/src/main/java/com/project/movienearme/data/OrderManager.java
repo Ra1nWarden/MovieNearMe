97e69d3d6f3c7c712fb007d624baf6897e3c8245
Ra1nWarden <wzh19921016@gmail.com>
2016-04-30 10:38:51 -0400

    Added cancel order flow.

--- app/src/main/java/com/project/movienearme/data/OrderManager.java
+++ app/src/main/java/com/project/movienearme/data/OrderManager.java
@@ -1,7 +1,21 @@
 package com.project.movienearme.data;
 
+import android.content.Context;
+import android.database.Cursor;
+
 /**
  * Order manager for managing orders.
  */
 public final class OrderManager {
+
+    private DatabaseOpenHelper databaseOpenHelper;
+
+    public OrderManager(Context context) {
+        this.databaseOpenHelper = new DatabaseOpenHelper(context);
+    }
+
+    public boolean removeOrder(int listingId, String username) {
+        return databaseOpenHelper.getWritableDatabase().delete("orders", "username = ? and " +
+                "listing_id = ?", new String[]{username, Integer.toString(listingId)}) > 0;
+    }
 }
--- app/src/main/java/com/project/movienearme/ui/OrderListFragment.java
+++ app/src/main/java/com/project/movienearme/ui/OrderListFragment.java
@@ -1,19 +1,23 @@
 package com.project.movienearme.ui;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.support.v4.app.ListFragment;
 import android.support.v7.app.AlertDialog;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.project.movienearme.R;
 import com.project.movienearme.data.MovieManager;
 import com.project.movienearme.data.OrderListAdapter;
+import com.project.movienearme.data.OrderManager;
 import com.project.movienearme.data.UserManager;
 
 import java.util.List;
@@ -27,8 +31,9 @@ public final class OrderListFragment extends ListFragment {
     public void onListItemClick(ListView l, View view, int position, long id) {
         super.onListItemClick(l, view, position, id);
         OrderListAdapter.ViewHolder viewHolder = (OrderListAdapter.ViewHolder) view.getTag();
-        MovieManager manager = new MovieManager(getActivity());
-        int listingId = viewHolder.listingId;
+        final MovieManager manager = new MovieManager(getActivity());
+        final OrderManager orderManager = new OrderManager(getActivity());
+        final int listingId = viewHolder.listingId;
         LayoutInflater inflater = LayoutInflater.from(getActivity());
         LinearLayout forms = (LinearLayout) inflater.inflate(R.layout.order_detail, null);
         TextView movieName = (TextView) forms.findViewById(R.id.movie_name_value);
@@ -41,8 +46,7 @@ public final class OrderListFragment extends ListFragment {
         timeField.setText(String.format("%02d:%02d", viewHolder.showTime / 60, viewHolder
                 .showTime % 60));
         TextView seatField = (TextView) forms.findViewById(R.id.seat_value);
-        StringBuilder seatString = new StringBuilder();
-        UserManager userManager = new UserManager(getActivity());
+        final UserManager userManager = new UserManager(getActivity());
         List<String> seats = manager.getSeatsForUsernameAndListingId(userManager
                 .getLoggedInUsername(), listingId);
         seatField.setText(TextUtils.join(",", seats));
@@ -54,8 +58,35 @@ public final class OrderListFragment extends ListFragment {
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.dismiss();
                     }
+                })
+                .setNegativeButton(R.string.cancel_order, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (orderManager.removeOrder(listingId, userManager.getLoggedInUsername()
+                        )) {
+                            toast(getActivity(), R.string.cancel_success);
+                        } else {
+                            toast(getActivity(), R.string.cancel_failure);
+                        }
+                        OrderListAdapter listAdapter = new OrderListAdapter(getActivity(),
+                                manager.getOrdersForUsername
+                                (userManager.getLoggedInUsername()));
+                        OrderListFragment.this.setListAdapter(listAdapter);
+                        dialog.dismiss();
+                    }
                 });
         Dialog dialog = builder.create();
         dialog.show();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d("TEST", "onResume()");
+    }
+
+    private void toast(Context context, int failedTextId) {
+        String failedText = context.getResources().getString(failedTextId);
+        Toast.makeText(context, failedText, Toast.LENGTH_SHORT).show();
     }
 }
